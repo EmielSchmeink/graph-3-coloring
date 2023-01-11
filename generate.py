eddcd0c0eb8fece7a0d343ec08d10d4fc3038f67
Emiel Schmeink <eschmeink@deloitte.nl>
2023-01-11 19:56:43 +0100

    small changes

--- generate.py
+++ generate.py
@@ -9,12 +9,12 @@ configurations = [
     # [0.8, None, 3, True, None],
     # [0.3, 4, 5, None, None],
     # [0.5, 4, 5, None, None],
-    [0.8, 4, None, None, None],
+    # [0.8, 4, 5, None, None],
     # [0.3, 7, 3, None, None],
     # [0.5, 7, 3, None, None],
-    # [0.8, 7, 3, None, None],
+    [0.8, 7, 3, None, None],
 ]
 
 for config in configurations:
-    for n in [50]:
+    for n in [30]:
         g = graph_generator.find_graphs_with_conditions(n, config[0], config[1], config[2], config[3], config[4])
