8ab006be2860f4639271fecfd4c8ae61de8262ed
Emiel Schmeink <eschmeink@deloitte.nl>
2023-01-12 14:23:22 +0100

    added print for not okay edge

--- generate.py
+++ generate.py
@@ -7,14 +7,15 @@ configurations = [
     # [0.3, None, 3, True, None],
     # [0.5, None, 3, True, None],
     # [0.8, None, 3, True, None],
+    [0.3, 4, 5, None, None],
     # [0.3, 4, 5, None, None],
     # [0.5, 4, 5, None, None],
     # [0.8, 4, 5, None, None],
     # [0.3, 7, 3, None, None],
     # [0.5, 7, 3, None, None],
-    [0.8, 7, 3, None, None],
+    # [0.8, 7, 3, None, None],
 ]
 
 for config in configurations:
-    for n in [50]:
+    for n in [20]:
         g = graph_generator.find_graphs_with_conditions(n, config[0], config[1], config[2], config[3], config[4])
